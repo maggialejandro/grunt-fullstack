733513f0955aff86867306c81641350e369dac9b
Alejandro M <alejandro.maggi@hotmail.com>
2014-03-15 10:57:26 -0300

    Express
    Configuraciones varias

---
+++ server.js
@@ -0,0 +1,44 @@
+'use strict';
+
+// Module dependencies
+var express = require('express'),
+	  http = require('http');
+
+/**
+ * Main application file
+ */
+
+// Set default node environment to development
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+
+var app = express();
+
+// Express settings
+require('./lib/config/express')(app);
+
+// Sequelize
+//var sequelize = require('./lib/db/sequelize');
+
+/* Sequelize Sync
+sequelize
+	.sequelize
+  	.sync({force:true})
+  	.complete(function(err) {
+    	if (err) {
+     		throw err
+    	} else {
+      		console.log('Sequelize sync complete');
+    	}
+  	});
+*/
+
+// Routing
+require('./lib/routes')(app);
+
+// Start server
+http.createServer(app).listen(app.get('port'), function() {
+	console.log('Express server listening on port %d in %s mode', app.get('port'), app.get('env'));
+});
+
+// Expose app
+exports = module.exports = app;
